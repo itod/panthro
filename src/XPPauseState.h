57a910b97413d69b917e834143a4078fc8bf239a
Todd Ditchendorf <todd.ditchendorf@gmail.com>
2014-07-10 14:24:45 -0500

    * pausestate. not used

---
+++ src/XPPauseState.h
@@ -0,0 +1,13 @@
+//
+//  XPPauseState.h
+//  Panthro
+//
+//  Created by Todd Ditchendorf on 7/10/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XPPauseState : NSObject
+
+@end
---
+++ src/XPPauseState.m
@@ -0,0 +1,13 @@
+//
+//  XPPauseState.m
+//  Panthro
+//
+//  Created by Todd Ditchendorf on 7/10/14.
+//
+//
+
+#import "XPPauseState.h"
+
+@implementation XPPauseState
+
+@end
